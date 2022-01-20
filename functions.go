a7998dc1becf9a8ab34a47f754c876e3b7305d2f
pranav verma <pranav12verma@gmail.com>
2022-01-20 13:25:55 +0530

    Added the rest of the 2 funtionality in funtions.go

--- functions.go
+++ functions.go
@@ -8,3 +8,31 @@ func Fun1(s string, slice []string) bool {
 	}
 	return false
 }
+
+func Fun2(slice1 []string, slice2 []string) []string {
+	var my_map = make(map[string]int)
+	var res []string
+	for _, val := range slice2 {
+		my_map[val] = my_map[val] + 1
+	}
+	for _, val := range slice1 {
+		if my_map[val] == 0 {
+			res = append(res, val)
+		}
+	}
+	return res
+}
+
+func Fun3(slice []string) []string {
+	var my_map = make(map[string]int)
+	var res []string
+	for _, val := range slice {
+		if my_map[val] == 0 {
+			my_map[val] = 1
+		}
+	}
+	for id, _ := range my_map {
+		res = append(res, id)
+	}
+	return res
+}
